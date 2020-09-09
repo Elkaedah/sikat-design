ed5862b982e2da4f16df84f39d818a39d300056c
derry <derry.ramadhan3@gmail.com>
2020-09-09 11:44:54 +0700

    commit on "Derry-Project"

---
+++ src/component/tabel.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {Table} from "react-bootstrap";
+
+
+export default class Tabel extends React.Component{
+render(){ 
+    return(
+        <div className="cardTable">
+        <Table striped bordered hover>
+          <thead className="border-top-0">
+            <tr>
+              <th>Nama</th>
+              <th>Kategori</th>
+              <th>Stok</th>
+              <th>Supplier</th>
+              <th>Status</th>
+              <th>Expired</th>
+              <th>Opsi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Indomie Goreng</td>
+              <td>Makanan</td>
+              <td>1530</td>
+              <td>PT.Bantuan Sosial</td>
+              <td>Status</td>
+              <td>29-12-2023</td>
+              <td>
+                <Link to="/EditDataLogistik">
+                  <a href="#" class="btn btn-warning edit">
+                    <img src={icoEdit} alt="edit" className="icoOption" />
+                  </a>
+                </Link>
+                <a href="#" class="btn btn-danger delete">
+                  <img src={icoDelete} alt="delete" className="icoOption" />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Indomie Goreng</td>
+              <td>Makanan</td>
+              <td>1530</td>
+              <td>PT.Bantuan Sosial</td>
+              <td>Status</td>
+              <td>29-12-2023</td>
+              <td>
+                <a href="#" class="btn btn-warning edit">
+                  <img src={icoEdit} alt="edit" className="icoOption" />
+                </a>
+                <a href="#" class="btn btn-danger delete">
+                  <img src={icoDelete} alt="delete" className="icoOption" />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Indomie Goreng</td>
+              <td>Makanan</td>
+              <td>1530</td>
+              <td>PT.Bantuan Sosial</td>
+              <td>Status</td>
+              <td>29-12-2023</td>
+              <td>
+                <a href="#" class="btn btn-warning edit">
+                  <img src={icoEdit} alt="edit" className="icoOption" />
+                </a>
+                <a href="#" class="btn btn-danger delete">
+                  <img src={icoDelete} alt="delete" className="icoOption" />
+                </a>
+              </td>
+            </tr>
+          </tbody>
+          </Table>
+          </div>
+    );
+}
+}
